4f4bbf10a458e60f3d62facd17b23e53033b2790
Murtuza Saifee <msaifee@firstorion.com>
2025-08-08 17:26:32 +0530

    Python implementation for simple tokenizer

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from llmforge!")
+from src.llmforge.tokenizer.SimpleTokenizer import SimpleTokenizer
+from src.llmforge.tokenizer.VocabLoader import vocab_loader
 
+def main():
+    test_simple_tokenizer()
+
+def test_simple_tokenizer():
+    vocab = vocab_loader()
+    tokenizer = SimpleTokenizer(vocab)
+
+    text = """"It's the last he painted, you know," 
+            Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(text)
+    print(ids)
+    
+    decoded_text = tokenizer.decode(ids)
+    print(decoded_text)
 
 if __name__ == "__main__":
     main()
---
+++ src/llmforge/tokenizer/VocabLoader.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import re
+
+def vocab_loader():
+    # Find the project root by looking for pyproject.toml
+    current_path = Path.cwd()
+    project_root = current_path
+    while not (project_root / "pyproject.toml").exists() and project_root != project_root.parent:
+        project_root = project_root.parent
+
+    data_file = project_root / "src" / "llmforge" / "data" / "training" / "the-verdict.txt"
+
+    with open(data_file, "r", encoding="utf-8") as f:
+        raw_text = f.read()
+        
+    print("Total number of character:", len(raw_text))
+    print(raw_text[:99])
+
+    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+    preprocessed = [item.strip() for item in preprocessed if item.strip()]
+    print(preprocessed[:30])
+
+    all_words = sorted(set(preprocessed))
+    vocab_size = len(all_words)
+
+    print(vocab_size)
+
+    vocab = {token:integer for integer,token in enumerate(all_words)}
+    
+    return vocab
