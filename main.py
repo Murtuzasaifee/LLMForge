4de9677e8055dc3c36fb95ad93d600275159fa62
Murtuza Saifee <msaifee@firstorion.com>
2025-08-18 15:10:12 +0530

    Testing text outside vocab

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from src.llmforge.tokenizer.SimpleTokenizer import SimpleTokenizer
 from src.llmforge.tokenizer.VocabLoader import vocab_loader
 
 def main():
-    test_simple_tokenizer()
+    # test_simple_tokenizer()
+    test_tokens_outside_vocab()
 
 def test_simple_tokenizer():
     vocab = vocab_loader()
@@ -10,11 +11,23 @@ def test_simple_tokenizer():
 
     text = """"It's the last he painted, you know," 
             Mrs. Gisburn said with pardonable pride."""
+    
     ids = tokenizer.encode(text)
     print(ids)
     
     decoded_text = tokenizer.decode(ids)
     print(decoded_text)
+
+
+def test_tokens_outside_vocab():
+    vocab = vocab_loader()
+    tokenizer = SimpleTokenizer(vocab)
+    
+    text = "Hello, do you like tea. Is this-- a test?"
+    
+    ids = tokenizer.encode(text)
+    print(ids)
+    
 
 if __name__ == "__main__":
     main()
